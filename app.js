85ac3d80700d87152629000cc844a19339181b61
naveen834 <75559553+naveen834@users.noreply.github.com>
2021-01-08 05:24:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ class UI {
   <div>    
     <h4>${item.title}</h4>
     <h5>$${item.price}</h5>
-    <span class="remove-item" data-id=${item.id}>remove</span>
+    <span class="remove-item" style="color:red" data-id=${item.id}>remove</span>
   </div>
   <div>
     <i class="fas fa-chevron-up" data-id=${item.id}></i>
